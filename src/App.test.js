2c75afdad64b6d0912ef1e2f97aae1193a29e177
Bernard Baker <bernard.baker.me@gmail.com>
2019-06-26 11:58:20 +0100

    Setup React Testing Library.
    https://github.com/bernardbaker/star-wars-top-trumps/issues/1

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { render, waitForElement } from "@testing-library/react";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("<App/>", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("renders default boilerplate text", async () => {
+    // Render new instance in every test to prevent leaking state
+    const { getByText } = render(<App />);
+
+    await waitForElement(() => getByText(/learn react/i));
+  });
 });
