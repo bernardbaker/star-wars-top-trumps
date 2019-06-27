cf2fe0067da4508a1590e6d7778a16e5d2bde4ff
Bernard Baker <bernard.baker.me@gmail.com>
2019-06-27 12:05:04 +0100

    Updated tests to wait for data to be retrieved from SWAPI.
    https://github.com/bernardbaker/star-wars-top-trumps/issues/3

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,11 @@ import React from "react";
 import App from "./App";
 import { render, waitForElement } from "@testing-library/react";
 import "@testing-library/react/cleanup-after-each";
+import { JestEnvironment } from "@jest/environment";
 
 describe("<App/>", () => {
   it("renders a list of cards", async () => {
+    jest.setTimeout(10000);
     // Act
     const { getAllByTestId } = render(<App />);
     // Assert
